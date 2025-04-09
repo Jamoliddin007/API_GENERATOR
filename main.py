0975f9127b937e6f454d73018eff87387c5068b2
jamoliddin007 <jamoliddin_007>
2025-04-09 15:47:37 +0500

    for main.py router is added

--- main.py
+++ main.py
@@ -1,18 +1,14 @@
 from fastapi import FastAPI
-from app.core.config import settings
-from app.routers import project
 from app.database.db_session import Base, engine
 from app.routers import crud
-from app.routers import news,project,products
 
-
+from app.routers import crud, project
 
 Base.metadata.create_all(bind=engine)
 
-app = FastAPI(title=settings.PROJECT_NAME)
+app = FastAPI(title="API Generator")
+
 
 app.include_router(project.router)
-app.include_router(crud.router)
-app.include_router(news.router)
-app.include_router(project.router)
-app.include_router(products.router)
+
+app.include_router(crud.router)  # crud routeri
