e8cbe23a7d63f89d5346b3bb44524d365b1dddd8
jamoliddin007 <jamoliddin_007>
2025-04-09 16:38:24 +0500

    prefix=/project

--- main.py
+++ main.py
@@ -2,13 +2,14 @@ from fastapi import FastAPI
 from app.database.db_session import Base, engine
 from app.routers import crud
 
-from app.routers import crud, project
+from app.routers import crud
+from app.routers.project import router as project_router
 
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI(title="API Generator")
 
 
-app.include_router(project.router)
+app.include_router(project_router, prefix="/project", tags=["Project"])
 
 app.include_router(crud.router)  # crud routeri
