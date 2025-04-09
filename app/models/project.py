bc53ee4ce261423b124428ea0e3b76dc8434ab90
jamoliddin007 <jamoliddin_007>
2025-04-09 15:57:24 +0500

    subdomain field is added

--- app/models/project.py
+++ app/models/project.py
@@ -6,3 +6,4 @@ class Project(Base):
 
     id = Column(Integer, primary_key=True, autoincrement=True)
     name = Column(String(255), nullable=False)
+    subdomain = Column(String(255), nullable=True)  
